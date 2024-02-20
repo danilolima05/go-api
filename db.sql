d7d339378e2fc18344d9ec89fd74451006516d57
Danilo Lima <danilo_costa_lima@hotmail.com>
2024-02-20 00:55:27 -0300

    fix sql script

--- db.sql
+++ db.sql
@@ -24,5 +24,5 @@ CREATE TABLE products (
 
 INSERT INTO `catalog`.`products` (`id`,`name`,`description`,`price`,`category_id`,`image_url`)
 VALUES ("7f8c9d8e-9f0a-1b2c-3d4e-5f6g7h8i9j0k","Product 1","Description 1", 100, "6b4c28f4-6831-495a-9444-19c93452faa3", "http://google.com/products/1.png"),
-("66805003-f9a2-4772-b577-d5ff66207707","Product 2","Description 2", 200, "6b4c28f4-6831-495a-9444-19c93452faa3", "http://google.com/products/2.png"),
+("66805003-f9a2-4772-b577-d5ff66207707","Product 2","Description 2", 200, "6b4c28f4-6831-495a-9444-19c93452faa3", "http://google.com/products/2.png");
 
